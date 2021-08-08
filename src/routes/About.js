32a95006ca699b938750b042e5cbab85034a827d
JnwnLee <james99322@gmail.com>
2021-08-09 02:05:36 +0900

    Repository ready

---
+++ src/routes/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./About.css"
+
+function About(props){
+    return <div className="about__container">
+        <span>
+            "Never was anything great achieved without danger."
+        </span>
+        <span>Niccolo Machiavelli</span>
+        <span>- A simple movie app for nomads -</span>
+    </div>;
+}
+
+export default About;
